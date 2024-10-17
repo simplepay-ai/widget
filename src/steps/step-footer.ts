c119155a2e754ba19b6cb630e4f6107b4280b3e4
Illya Ponomarenko <ponomarenko.illya97@gmail.com>
2024-10-17 18:28:55 +0300

    update footer box image

--- src/steps/step-footer.ts
+++ src/steps/step-footer.ts
@@ -114,8 +114,8 @@ export class StepFooter extends LitElement {
                                     <div class="product">
                                         <div class="image placeholder">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none">
-                                                <path d="M15.5777 3.38197L17.5777 4.43152C19.7294 5.56066 20.8052 6.12523 21.4026 7.13974C22 8.15425 22 9.41667 22 11.9415V12.0585C22 14.5833 22 15.8458 21.4026 16.8603C20.8052 17.8748 19.7294 18.4393 17.5777 19.5685L15.5777 20.618C13.8221 21.5393 12.9443 22 12 22C11.0557 22 10.1779 21.5393 8.42229 20.618L6.42229 19.5685C4.27063 18.4393 3.19479 17.8748 2.5974 16.8603C2 15.8458 2 14.5833 2 12.0585V11.9415C2 9.41667 2 8.15425 2.5974 7.13974C3.19479 6.12523 4.27063 5.56066 6.42229 4.43152L8.42229 3.38197C10.1779 2.46066 11.0557 2 12 2C12.9443 2 13.8221 2.46066 15.5777 3.38197Z" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
-                                                <path d="M21 7.5L17 9.5M12 12L3 7.5M12 12V21.5M12 12C12 12 14.7426 10.6287 16.5 9.75C16.6953 9.65237 17 9.5 17 9.5M17 9.5V13M17 9.5L7.5 4.5" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+                                                <path d="M15.5777 3.38197L17.5777 4.43152C19.7294 5.56066 20.8052 6.12523 21.4026 7.13974C22 8.15425 22 9.41667 22 11.9415V12.0585C22 14.5833 22 15.8458 21.4026 16.8603C20.8052 17.8748 19.7294 18.4393 17.5777 19.5685L15.5777 20.618C13.8221 21.5393 12.9443 22 12 22C11.0557 22 10.1779 21.5393 8.42229 20.618L6.42229 19.5685C4.27063 18.4393 3.19479 17.8748 2.5974 16.8603C2 15.8458 2 14.5833 2 12.0585V11.9415C2 9.41667 2 8.15425 2.5974 7.13974C3.19479 6.12523 4.27063 5.56066 6.42229 4.43152L8.42229 3.38197C10.1779 2.46066 11.0557 2 12 2C12.9443 2 13.8221 2.46066 15.5777 3.38197Z" stroke="#1C274C" stroke-width="1" stroke-linecap="round"/>
+                                                <path d="M21 7.5L17 9.5M12 12L3 7.5M12 12V21.5M12 12C12 12 14.7426 10.6287 16.5 9.75C16.6953 9.65237 17 9.5 17 9.5M17 9.5V13M17 9.5L7.5 4.5" stroke="#1C274C" stroke-width="1" stroke-linecap="round"/>
                                             </svg>
                                         </div>
 
@@ -144,8 +144,8 @@ export class StepFooter extends LitElement {
                                                             `
                                                             : html`
                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none">
-                                                                    <path d="M15.5777 3.38197L17.5777 4.43152C19.7294 5.56066 20.8052 6.12523 21.4026 7.13974C22 8.15425 22 9.41667 22 11.9415V12.0585C22 14.5833 22 15.8458 21.4026 16.8603C20.8052 17.8748 19.7294 18.4393 17.5777 19.5685L15.5777 20.618C13.8221 21.5393 12.9443 22 12 22C11.0557 22 10.1779 21.5393 8.42229 20.618L6.42229 19.5685C4.27063 18.4393 3.19479 17.8748 2.5974 16.8603C2 15.8458 2 14.5833 2 12.0585V11.9415C2 9.41667 2 8.15425 2.5974 7.13974C3.19479 6.12523 4.27063 5.56066 6.42229 4.43152L8.42229 3.38197C10.1779 2.46066 11.0557 2 12 2C12.9443 2 13.8221 2.46066 15.5777 3.38197Z" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
-                                                                    <path d="M21 7.5L17 9.5M12 12L3 7.5M12 12V21.5M12 12C12 12 14.7426 10.6287 16.5 9.75C16.6953 9.65237 17 9.5 17 9.5M17 9.5V13M17 9.5L7.5 4.5" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+                                                                    <path d="M15.5777 3.38197L17.5777 4.43152C19.7294 5.56066 20.8052 6.12523 21.4026 7.13974C22 8.15425 22 9.41667 22 11.9415V12.0585C22 14.5833 22 15.8458 21.4026 16.8603C20.8052 17.8748 19.7294 18.4393 17.5777 19.5685L15.5777 20.618C13.8221 21.5393 12.9443 22 12 22C11.0557 22 10.1779 21.5393 8.42229 20.618L6.42229 19.5685C4.27063 18.4393 3.19479 17.8748 2.5974 16.8603C2 15.8458 2 14.5833 2 12.0585V11.9415C2 9.41667 2 8.15425 2.5974 7.13974C3.19479 6.12523 4.27063 5.56066 6.42229 4.43152L8.42229 3.38197C10.1779 2.46066 11.0557 2 12 2C12.9443 2 13.8221 2.46066 15.5777 3.38197Z" stroke="#1C274C" stroke-width="1" stroke-linecap="round"/>
+                                                                    <path d="M21 7.5L17 9.5M12 12L3 7.5M12 12V21.5M12 12C12 12 14.7426 10.6287 16.5 9.75C16.6953 9.65237 17 9.5 17 9.5M17 9.5V13M17 9.5L7.5 4.5" stroke="#1C274C" stroke-width="1" stroke-linecap="round"/>
                                                                 </svg>
                                                             `
                                             }
@@ -317,8 +317,8 @@ export class StepFooter extends LitElement {
                                                             `
                                                                             : html`
                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none">
-                                                                    <path d="M15.5777 3.38197L17.5777 4.43152C19.7294 5.56066 20.8052 6.12523 21.4026 7.13974C22 8.15425 22 9.41667 22 11.9415V12.0585C22 14.5833 22 15.8458 21.4026 16.8603C20.8052 17.8748 19.7294 18.4393 17.5777 19.5685L15.5777 20.618C13.8221 21.5393 12.9443 22 12 22C11.0557 22 10.1779 21.5393 8.42229 20.618L6.42229 19.5685C4.27063 18.4393 3.19479 17.8748 2.5974 16.8603C2 15.8458 2 14.5833 2 12.0585V11.9415C2 9.41667 2 8.15425 2.5974 7.13974C3.19479 6.12523 4.27063 5.56066 6.42229 4.43152L8.42229 3.38197C10.1779 2.46066 11.0557 2 12 2C12.9443 2 13.8221 2.46066 15.5777 3.38197Z" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
-                                                                    <path d="M21 7.5L17 9.5M12 12L3 7.5M12 12V21.5M12 12C12 12 14.7426 10.6287 16.5 9.75C16.6953 9.65237 17 9.5 17 9.5M17 9.5V13M17 9.5L7.5 4.5" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+                                                                    <path d="M15.5777 3.38197L17.5777 4.43152C19.7294 5.56066 20.8052 6.12523 21.4026 7.13974C22 8.15425 22 9.41667 22 11.9415V12.0585C22 14.5833 22 15.8458 21.4026 16.8603C20.8052 17.8748 19.7294 18.4393 17.5777 19.5685L15.5777 20.618C13.8221 21.5393 12.9443 22 12 22C11.0557 22 10.1779 21.5393 8.42229 20.618L6.42229 19.5685C4.27063 18.4393 3.19479 17.8748 2.5974 16.8603C2 15.8458 2 14.5833 2 12.0585V11.9415C2 9.41667 2 8.15425 2.5974 7.13974C3.19479 6.12523 4.27063 5.56066 6.42229 4.43152L8.42229 3.38197C10.1779 2.46066 11.0557 2 12 2C12.9443 2 13.8221 2.46066 15.5777 3.38197Z" stroke="#1C274C" stroke-width="1" stroke-linecap="round"/>
+                                                                    <path d="M21 7.5L17 9.5M12 12L3 7.5M12 12V21.5M12 12C12 12 14.7426 10.6287 16.5 9.75C16.6953 9.65237 17 9.5 17 9.5M17 9.5V13M17 9.5L7.5 4.5" stroke="#1C274C" stroke-width="1" stroke-linecap="round"/>
                                                                 </svg>
                                                             `
                                                             }
@@ -656,57 +656,6 @@ export class StepFooter extends LitElement {
                     text-align: left;
                 }
             }
-
-            &.dark {
-                border-color: color-mix(
-                        in srgb,
-                        var(--sp-widget-hint-color) 15%,
-                        transparent
-                ) !important;
-                background: color-mix(
-                        in srgb,
-                        var(--sp-widget-secondary-bg-color) 15%,
-                        transparent
-                ) !important;
-
-                .layer1{
-                    background: black;
-                }
-                
-                .layer2{
-                    background: color-mix(
-                            in srgb,
-                            var(--sp-widget-secondary-bg-color) 15%,
-                            transparent
-                    ) !important;
-                }
-                
-                .product {
-                    .productsInfo {
-                        &:hover,
-                        &:active {
-                            background: color-mix(in srgb, var(--sp-widget-secondary-bg-color) 10%, transparent) !important;
-                        }
-                    }
-                }
-
-                .timerWrapper {
-                    .timerBg{
-                        stroke: color-mix(in srgb, var(--sp-widget-bg-color) 40%, transparent) !important;
-                    }
-                }
-                
-                .secondaryButton{
-                    color: var(--sp-widget-text-color) !important;
-                    background: var(--sp-widget-bg-color) !important;
-                    border-color: var(--sp-widget-bg-color) !important;
-                    
-                    &:hover,
-                    &:active{
-                        background: color-mix(in srgb, var(--sp-widget-bg-color) 50%, transparent) !important;
-                    }
-                }
-            }
         }
 
         .fullProductInfo {
@@ -903,46 +852,6 @@ export class StepFooter extends LitElement {
                                 }
                             }
                         }
-                    }
-                }
-            }
-            
-            &.dark{
-                .overlay {
-                    background: color-mix(
-                            in srgb,
-                            var(--sp-widget-bg-color) 0%,
-                            transparent
-                    ) !important;
-
-                    &.active {
-                        background: color-mix(
-                                in srgb,
-                                var(--sp-widget-bg-color) 75%,
-                                transparent
-                        ) !important;
-                    }
-                }
-
-                .contentWrapper{
-                    background: color-mix(
-                            in srgb,
-                            var(--sp-widget-hint-color) 100%,
-                            transparent
-                    ) !important;
-                    border-top: 1px solid color-mix(
-                            in srgb,
-                            var(--sp-widget-bg-color) 70%,
-                            transparent
-                    ) !important;
-                    
-                    .content{
-                        overflow: hidden;
-                        background: color-mix(
-                                in srgb,
-                                var(--sp-widget-bg-color) 85%,
-                                transparent
-                        ) !important;
                     }
                 }
             }
