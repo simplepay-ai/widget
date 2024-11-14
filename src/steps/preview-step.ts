1633f7d9803ede08b028fbaad95e86aacc8d2494
Illya Ponomarenko <ponomarenko.illya97@gmail.com>
2024-11-15 00:44:00 +0200

    fixs

--- src/steps/preview-step.ts
+++ src/steps/preview-step.ts
@@ -888,6 +888,7 @@ export class PreviewStep extends LitElement {
                         img {
                             width: 100%;
                             height: 100%;
+                            object-fit: cover;
                         }
                     }
 
